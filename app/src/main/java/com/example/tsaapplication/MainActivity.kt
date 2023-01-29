3f65ce458f49563aa4facd1fe71ca01751812be6
Alicia <Alicia@gmail.com>
2023-01-28 21:23:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tsaapplication/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tsaapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tapHereButton: Button
+    private lateinit var leftArrow: ImageButton
+    private lateinit var rightArrow: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tapHereButton = findViewById(R.id.tap_here_button)
+        leftArrow = findViewById(R.id.left_arrow)
+        rightArrow = findViewById(R.id.right_arrow)
+
+        tapHereButton.setOnClickListener {
+            showRandomArrow()
+        } // Call function when button is clicked
+
+        leftArrow.setOnClickListener {
+            reset() // Function clicks to hide left arrow.
+        }
+
+        rightArrow.setOnClickListener {
+            reset() // Function listens to hide right arrow.
+        }
+
+    }
+
+    private fun reset() {
+        leftArrow.visibility = View.INVISIBLE
+        rightArrow.visibility = View.INVISIBLE
+        tapHereButton.visibility = View.VISIBLE // Shows button only when tab
+    }
+
+    // Function is to hide tap here button and shows left or right arrow.
+    private fun showRandomArrow() {
+        tapHereButton.visibility = View.INVISIBLE
+        val direction = listOf("left", "right").random()
+        if ( direction == "left") {
+            leftArrow.visibility = View.VISIBLE
+        }else {
+           rightArrow.visibility = View.VISIBLE
+        }
+    }
+
+}
